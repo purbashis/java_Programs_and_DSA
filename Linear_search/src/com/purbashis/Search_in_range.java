6423ba8d1cfe859102f3fe314cbab2ba6de27f18
PURBASHIS BEHERA <purbashis01042000@gmail.com>
2023-02-14 10:36:08 +0530

    search in range
    search in range by using liner search .
    in this program there is an array given arr= {18,12,-7,3,14,28}
    you have to search for -7 in the range of index [1,5].

--- Linear_search/src/com/purbashis/Search_in_range.java
+++ Linear_search/src/com/purbashis/Search_in_range.java
@@ -2,6 +2,27 @@ package com.purbashis;
 
 public class Search_in_range {
     public static void main(String[] args) {
+    int[]arr={18,12,-7,3,14,28};
+      int target =-7;
+
+        int ans = linear_search(arr,target,1,5);
+        System.out.println(ans);
+
         
+    }
+    static  int linear_search (int [] arr, int  target,int start ,int end ){
+        if (arr.length==0){
+            return -1;
+        }
+        for (int index = start; index < end; index++) {
+
+            int element =arr[index];
+            if(element== target ){
+                return index;
+            }
+
+        }
+        return -1;
+
     }
 }
