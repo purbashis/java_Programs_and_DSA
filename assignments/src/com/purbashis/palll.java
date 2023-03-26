c66fe2aeb80d97d1f5083bd5c6a0365867fe6efe
PURBASHIS BEHERA <purbashis01042000@gmail.com>
2023-03-26 11:10:01 +0530

    palll

---
+++ assignments/src/com/purbashis/palll.java
@@ -0,0 +1,23 @@
+package com.purbashis;
+
+public class palll {
+    public static void main(String[] args) {
+        int num= 3;
+        int count = 0;
+        if(num>1){
+            for(int i =1;i<= num;i++){
+                if(num%i==0){
+                    count++;
+                }
+                if(count==2){
+                    System.out.println("its a prime number");
+                }
+            }
+        }
+        else {
+            System.out.println("its not a prime number .");
+        }
+
+
+    }
+}
